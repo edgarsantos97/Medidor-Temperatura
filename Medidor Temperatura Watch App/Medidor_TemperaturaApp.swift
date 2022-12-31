bd7e265821c3eabf63ae67510f96784840ecca68
edgarsantos97 <edgarsantos97@hotmail.com>
2022-12-31 16:56:50 +0000

    Initial Commit

---
+++ Medidor Temperatura Watch App/Medidor_TemperaturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medidor_TemperaturaApp.swift
+//  Medidor Temperatura Watch App
+//
+//  Created by Edgar Santos on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Medidor_Temperatura_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
